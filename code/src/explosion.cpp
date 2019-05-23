f45a163888019accfdab531fe0801c5c7d0efbae
serk12 <marc.prat.maso@est.fib.upc.edu>
2019-05-23 14:35:17 +0200

    fix: basic demo

--- code/src/explosion.cpp
+++ code/src/explosion.cpp
@@ -54,7 +54,7 @@ void Explosion::draw() {
     glDepthMask(GL_TRUE);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    glTranslatef(0.0, 0.0, -10.5);
+    glTranslatef(0.0, 0.0, 0.0);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glVertexPointer(4, GL_FLOAT, 0, 0);
